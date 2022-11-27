feaeba847405b487854e203405d705eb3b0fd4db
Michal Baran <michal.baran@live.com>
2022-11-27 21:22:28 +0100

    build: update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,18 @@
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   /** Expose public folder to storybook as static */
-  staticDirs: ['../public'],
+  // staticDirs: ['./public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  core: {},
+  docsPage: {
+    docs: 'automatic',
   },
 };
